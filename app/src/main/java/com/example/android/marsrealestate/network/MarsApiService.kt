e23b707ffd28c7f6a75a1309eddf66dd6a94b847
sultanov-be <sultanov_b@auca.kg>
2021-10-20 13:59:56 +0600

    Connect Libraries and Made first step to connecting to the Internet.

---
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.android.marsrealestate.network
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://mars.udacity.com/" // for getting response from the JSON and returning it as a string
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)// specifying the root-path
+    .build() // to create a retrofit object
+
+interface MarsApiService {
+    @GET("realestate") // specifying an end point
+    fun getProperties():
+            Call<String> // is used to start a request
+}
+
+object MarsApi {
+    val retrofitService : MarsApiService by lazy { // return Retorfit that release a MarsApiService
+        retrofit.create(MarsApiService::class.java)
+    }
+}
